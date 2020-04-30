fe390922382a53e7b81515da59d26b91712d7a3d
hyird <1326606797@qq.com>
2020-04-30 19:49:10 +0800

    .

---
+++ user/common/custom.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Apply custom.sh"
+
+sed -i '/lienol/d'                         package/default-settings/files/zzz-default-settings
+sed -i '/shadow/d'                     package/default-settings/files/zzz-default-settings
+sed -i "s/#sed/sed/g"                package/default-settings/files/zzz-default-settings
+sed -i "s/openwrt.proxy.ustclug.org/op.hyird.xyz/g"  package/default-settings/files/zzz-default-settings
+sed -i "s/https/4-26/g"  package/default-settings/files/zzz-default-settings
+sed -i  's/http/releases\\\/19.07\-SNAPSHOT/g'  package/default-settings/files/zzz-default-settings
+
+git clone https://github.com/fw876/helloworld.git  package/ssr
+git clone https://github.com/hyird/passwall.git    package/passwall
+svn checkout https://github.com/coolsnowwolf/lede/trunk/package/lean/redsocks2   package/redsocks2
+svn checkout https://github.com/coolsnowwolf/lede/trunk/package/lean/tcpping     package/tcpping
+svn checkout https://github.com/coolsnowwolf/lede/trunk/package/lean/microsocks  package/microsocks
+
