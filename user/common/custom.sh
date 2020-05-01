82c61f18317c64001069403f40fe6e2ee8e17aab
hyird <1326606797@qq.com>
2020-05-01 17:16:40 +0800

    .

--- user/common/custom.sh
+++ user/common/custom.sh
@@ -2,16 +2,14 @@
 
 echo "Apply custom.sh"
 
-sed -i '/lienol/d'                         package/default-settings/files/zzz-default-settings
-sed -i '/shadow/d'                     package/default-settings/files/zzz-default-settings
-sed -i "s/#sed/sed/g"                package/default-settings/files/zzz-default-settings
-sed -i "s/openwrt.proxy.ustclug.org/op.hyird.xyz/g"  package/default-settings/files/zzz-default-settings
-sed -i "s/https/4-26/g"  package/default-settings/files/zzz-default-settings
-sed -i  's/http/releases\\\/19.07\-SNAPSHOT/g'  package/default-settings/files/zzz-default-settings
+sed -i '/shadow/d'                     package/lean/default-settings/files/zzz-default-settings
+sed -i "s/openwrt.proxy.ustclug.org/op.hyird.xyz/g"  package/lean/default-settings/files/zzz-default-settings
+sed -i "s/https:/R20.0.0/g"  package/lean/default-settings/files/zzz-default-settings
+sed -i  's/http:/snapshots/g'  package/lean/default-settings/files/zzz-default-settings
+sed -i 's/R20\(.[0-9].[0-9]\)/R20.5.1/g' package/lean/default-settings/files/zzz-default-settings
 
-git clone https://github.com/fw876/helloworld.git  package/ssr
-git clone https://github.com/hyird/passwall.git    package/passwall
-svn checkout https://github.com/coolsnowwolf/lede/trunk/package/lean/redsocks2   package/redsocks2
-svn checkout https://github.com/coolsnowwolf/lede/trunk/package/lean/tcpping     package/tcpping
-svn checkout https://github.com/coolsnowwolf/lede/trunk/package/lean/microsocks  package/microsocks
-
+git clone https://github.com/hyird/passwall.git                     package/passwall
+glt clone https://github.com/Mrbai98/luci-theme-atmaterial.git      package/luci-theme-atmaterial
+git clone https://github.com/vernesong/OpenClash.git                package/clash
+svn checkout https://github.com/Lienol/openwrt/trunk/package/diy/luci-app-adguardhome package/adg
+ 
