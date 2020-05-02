b835a7f670b52bee0ab48aa40ce135d0977baac9
hyird <31695447+hyird@users.noreply.github.com>
2020-05-02 14:04:34 +0800

    Update custom.sh

--- user/lienol-x64/custom.sh
+++ user/lienol-x64/custom.sh
@@ -2,13 +2,13 @@
 
 echo "Test custom.sh"
 
-source ../../version
+source ../version
 
 sed -i '/lienol/d'                         package/default-settings/files/zzz-default-settings
 sed -i '/shadow/d'                     package/default-settings/files/zzz-default-settings
 sed -i "s/#sed/sed/g"                package/default-settings/files/zzz-default-settings
 sed -i "s/openwrt.proxy.ustclug.org/op.hyird.xyz/g"  package/default-settings/files/zzz-default-settings
-sed -i "s/https/'$version'/g"  package/default-settings/files/zzz-default-settings
+sed -i "s/https/$version/g"  package/default-settings/files/zzz-default-settings
 sed -i  's/http/releases\\\/19.07\-SNAPSHOT/g'  package/default-settings/files/zzz-default-settings
 
 git clone https://github.com/fw876/helloworld.git  package/ssr
