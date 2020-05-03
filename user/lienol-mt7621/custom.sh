177a61c6d765f2884cdfa6a9f9bfa1aab503b0e5
hyird <1326606797@qq.com>
2020-05-03 12:34:31 +0800

    add mt7621

---
+++ user/lienol-mt7621/custom.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "Test custom.sh"
+
+source ../version
+
+sed -i '/lienol/d'                         package/default-settings/files/zzz-default-settings
+sed -i '/shadow/d'                     package/default-settings/files/zzz-default-settings
+sed -i "s/#sed/sed/g"                package/default-settings/files/zzz-default-settings
+sed -i "s/openwrt.proxy.ustclug.org/op.hyird.xyz/g"  package/default-settings/files/zzz-default-settings
+sed -i "s/https/$version/g"  package/default-settings/files/zzz-default-settings
+sed -i  's/http/releases\\\/19.07\-SNAPSHOT/g'  package/default-settings/files/zzz-default-settings
+
+git clone https://github.com/fw876/helloworld.git  package/ssr
+git clone https://github.com/hyird/passwall.git    package/passwall
+svn checkout https://github.com/coolsnowwolf/lede/trunk/package/lean/redsocks2   package/redsocks2
+svn checkout https://github.com/coolsnowwolf/lede/trunk/package/lean/tcpping     package/tcpping
+svn checkout https://github.com/coolsnowwolf/lede/trunk/package/lean/microsocks  package/microsocks
+
